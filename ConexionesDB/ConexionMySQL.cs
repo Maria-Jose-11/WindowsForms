684ca4dfb6b614bd9a6e975d1c8c617cd1b3ad1c
Majo Chalá <74751902+Maria-Jose-11@users.noreply.github.com>
2024-04-25 22:53:54 -0500

    Conexiones MySQL y SQL Server

---
+++ ConexionesDB/ConexionMySQL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConexionesDB
+{
+    public class ConexionMySQL
+    {
+        private void ConexionBaseMySQL()
+        {
+            //Formato cadena de conexion: "server=tu_servidor;port=tu_puerto;database=tu_base_de_datos;uid=tu_usuario;password=tu_contraseña;";
+            string connectionString = "tu_cadena_de_conexion";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                
+                string sqlQuery = "SELECT * FROM empleados";
+                using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string nombre = reader.GetString(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConexionesDB/ConexionSQLServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConexionesDB
+{
+
+
+    //Para conectar una aplicación Windows Forms a una base de datos SQL Server, puedes seguir estos pasos básicos:
+
+    //Agregar un Controlador de Datos:
+    //En tu proyecto de Windows Forms en Visual Studio, puedes agregar un controlador de datos para facilitar la conexión y el acceso a la base de datos SQL Server.Puedes hacerlo desde el menú "Datos" -> "Agregar nuevo origen de datos" y seguir el asistente para establecer la conexión.
+    //Establecer la Cadena de Conexión:
+    //Asegúrate de tener la cadena de conexión correcta para tu base de datos SQL Server. La cadena de conexión contendrá información como el servidor, la base de datos, el nombre de usuario y la contraseña (si es necesario). Puedes establecer esta cadena de conexión en el código o en la configuración de la aplicación.
+    //Abrir la Conexión y Ejecutar Consultas:
+    //Una vez que tienes la cadena de conexión configurada, puedes abrir una conexión a la base de datos utilizando la clase SqlConnection. Luego, puedes ejecutar consultas SQL utilizando objetos como SqlCommand y SqlDataReader.
+    public class ConexionSQLServer
+    {
+        private void ConexionBaseSQLServer()
+        {
+
+            //FORMATO PARA CADENA DE CONEXIÓN: Data Source=tu_servidor;Initial Catalog=tu_base_de_datos;User ID=tu_usuario;Password=tu_contraseña;
+
+            string connectionString = "tu_cadena_de_conexion";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Ejemplo de consulta
+                string sqlQuery = "SELECT * FROM empleados";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            
+                            string nombre = reader.GetString(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
